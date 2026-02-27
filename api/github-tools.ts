80030640a26625c0ef64bb93b1aacbd551d105f2
Ritesh Yadav <ry3729819@gmail.com>
2026-02-27 22:37:29 +0530

    fix api response array

--- api/github-tools.ts
+++ api/github-tools.ts
@@ -1,9 +1,3 @@
 export default async function handler(req, res) {
-    try {
-        res.status(200).json({
-            message: "GitHub tools API working"
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Server error" });
-    }
+    res.status(200).json([]);
 }
